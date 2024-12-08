502ab6e61d7925a77bb679b0e357d12d219ae46a
= <54559465s@iespoblenou.org>
2024-12-08 20:34:12 +0100

    Add divide method

--- Calculator.java
+++ Calculator.java
@@ -8,10 +8,17 @@ public class Calculator {
     public int multiply(int a, int b) {
         return a * b;
     }
+    public int divide(int a, int b){
+        if (b == 0) {
+            throw new ArithmeticException("Division by zero is not allowed.");
+        }
+        return a / b;
+    }
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         System.out.println("Addition: " + calculator.add(10, 5));
         System.out.println("Subtraction: " + calculator.subtract(10, 5));
         System.out.println("Multiply: " + calculator.multiply(10, 5));
+        System.out.println("Multiply: " + calculator.divide(10, 5));
     }
 }
